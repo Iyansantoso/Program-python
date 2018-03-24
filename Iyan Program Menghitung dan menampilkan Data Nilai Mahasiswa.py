2d138f4fa7cf1d1728bc59db27fd0abb44feeadb
Iyansantoso <37440760+Iyansantoso@users.noreply.github.com>
2018-03-24 08:00:01 +0700

    Update Iyan Program Menghitung dan menampilkan Data Nilai Mahasiswa.py

--- Iyan Program Menghitung dan menampilkan Data Nilai Mahasiswa.py
+++ Iyan Program Menghitung dan menampilkan Data Nilai Mahasiswa.py
@@ -1,3 +1,5 @@
+from texttable import Texttable
+table = Texttable ()
 jawab = "y"
 no = 0
 nama = []
@@ -13,13 +15,11 @@ while (jawab == "y"):
     n_uas.append(input("Nilai UAS :"))
     jawab = input ("Tambah data (y/t)?")
     no +=1
-print("=======================================================================")
-print("| No |     Nama     |     NIM     | Tugas |  UTS  |  UAS  |   Akhir   |")
-print("=======================================================================")
-for n in range(no):
-    nt = int(n_tugas[n])
-    nu = int(n_uts[n])
-    na = int(n_uas[n])
+for i in range(no):
+    nt = int(n_tugas[i])
+    nu = int(n_uts[i])
+    na = int(n_uas[i])
     akhir = (nt*30/100) + (nu*35/100) + (na*35/100)
-    print("| {} | {}   | {}   | {}   | {}   | {}   | {}   |".format(n+1,nama[n],nim[n],n_tugas[n],n_uts[n],n_uas
-    [n],akhir))
+    table.add_rows([['No','Nama','NIM','TUGAS','UTS','UAS','AKHIR'],
+                    [i+1,nama[i],nim[i],n_tugas[i],n_uts[i],n_uas[i],akhir]])
+print (table.draw())
